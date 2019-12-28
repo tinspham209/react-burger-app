f5541dc59ba4ca3cd40118245c11ccd9b317daf5
tinspham <tindeptrai1@gmail.com>
2019-12-28 15:03:38 +0700

    Fetching Orders via Redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -29,10 +29,26 @@ const reducer = (state = initialState, action) => {
         loading: false
       };
     case actionTypes.PURCHASE_INIT:
-      return{
+      return {
         ...state,
         purchased: false
-      }
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        orders: action.orders
+      };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }
