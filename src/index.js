3035de5fc95f70da58b30d41be9ae1a6c052a8af
tinspham <tindeptrai1@gmail.com>
2020-01-10 08:26:56 +0700

    Moving BurgerBuilder - initIngredients into Saga

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
-import { watchAuth } from "./store/sagas";
+import { watchAuth, watchBurgerBuilder } from "./store/sagas";
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -32,6 +32,8 @@ const store = createStore(
 );
 
 sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+
 
 const app = (
   <Provider store={store}>
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -3,6 +3,7 @@ import { put } from "redux-saga/effects";
 import axios from "axios";
 
 import * as actions from "../actions/index";
+
 export function* logoutSaga(action) {
   yield localStorage.removeItem("token");
   yield localStorage.removeItem("expirationDate");
@@ -42,5 +43,27 @@ export function* authUserSaga(action) {
     yield put(actions.checkAuthTimeout(response.data.expiresIn));
   } catch (error) {
     yield put(action.authFail(error.response.data.error));
+  }
+}
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem("token");
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date(
+      localStorage.getItem("expirationDate")
+    );
+    if (expirationDate <= new Date()) {
+      yield put(actions.logout());
+    } else {
+      const userId = yield localStorage.getItem("userId");
+      yield put(actions.authSuccess(token, userId));
+      yield put(
+        actions.checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
+    }
   }
 }
