4877c2ca9b425dccd96579a6fe11ee75b05e5f77
tinspham <tindeptrai1@gmail.com>
2020-01-16 11:29:51 +0700

    Add Hooks to Logout containers

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,26 +1,23 @@
-import React , {Component} from 'react';
-import {Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import * as actions from "../../../store/actions/index";
-class Logout extends Component {
-     componentDidMount() {
-          this.props.onLogout();
-     }
-     
+const logout = props => {
+  useEffect(() => {
+    props.onLogout();
+  }, []);
 
-     render() {
-          return (
-               <div>
-                    <Redirect to="/"/>
-               </div>
-          )
-     }
-}
+  return (
+    <div>
+      <Redirect to="/" />
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => {
-     return{
-          onLogout: () => dispatch(actions.logout())
-     }
-}
+  return {
+    onLogout: () => dispatch(actions.logout())
+  };
+};
 
-export default connect(null,mapDispatchToProps)(Logout);
+export default connect(null, mapDispatchToProps)(logout);
